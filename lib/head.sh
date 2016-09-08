61bd85f7bd49bcb652fb59cb989d687d83a511e2
Andreas Eiermann <andreas@firecyberice.de>
2016-09-08 11:17:41 +0200

    sort and add more default values

--- lib/head.sh
+++ lib/head.sh
@@ -12,7 +12,12 @@ if [ -f ./config.sh ]; then
     . ./config.sh
 fi
 
-[ ${PROJECT:-} ] && PROJECT="-p $PROJECT"
-: ${NETWORK:="caddy_backend"}
 : ${CADDY_DIR:="caddy"}
 : ${SERVICES_DIR:="services"}
+: ${MAIL:="noreply@domain.tld"}
+: ${FQDN:="domain.tld"}
+
+
+[ ${PROJECT:-} ] && PROJECT="-p $PROJECT"
+: ${NETWORK:="caddy_backend"}
+: ${CADDY_IMAGENAME:="fciserver/caddy"}
